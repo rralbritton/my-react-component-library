b52c4216f3a02c08f505993f1fdff77fc99ef8ee
Rachel Albritton <ralbritton@utah.gov>
2020-02-15 07:35:53 -0700

    added FIlterBasic - needs better css

---
+++ src/components/FilterBasic/FilterBasic.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+export default class SimpleFilter extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            search: "",
+        };
+    }
+
+    //The subsr limits the # of characters a user can enter into the seach box
+    updateSearch = event => {
+        this.setState({ search: event.target.value.substr(0, 10) });
+    };
+
+    render() {
+        let filteredList = this.props.list.filter(item => {
+            return item.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+        });
+
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={this.state.search}
+                    onChange={this.updateSearch.bind(this)}
+                    placeholder="Enter Text Here..."
+                    className="form-control"
+                    aria-describedby="searchbox"
+                />
+                <ul>
+                    {filteredList.map(item => {
+                        return <li key={item}>{item}</li>;
+                    })}
+                </ul>
+            </div>
+        );
+    }
+}
---
+++ src/docs/examples/FilterBasic/ExampleFilterBasic.js
@@ -0,0 +1,8 @@
+import React from "react";
+import FilterBasic from "react-component-library/FilterBasic";
+
+export default function ExampleFilterBasic() {
+    return (
+        <FilterBasic list={["one", "two", "three", "four", "five"]} />
+    )
+}
